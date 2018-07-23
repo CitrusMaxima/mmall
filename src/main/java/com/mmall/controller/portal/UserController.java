5699e63bcf725ea4e3c7f2f3f85a43c496bfb0b8
CitrusMaxima <2663753900@qq.com>
2018-07-23 15:36:56 +0800

    获取用户详细信息功能实现

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,6 +1,7 @@
 package com.mmall.controller.portal;
 
 import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
@@ -169,6 +170,22 @@ public class UserController {
             session.setAttribute(Const.CURRENT_USER, response.getData());
         }
         return response;
+    }
+
+    /**
+     * 获取用户详细信息
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "get_information.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getInformation(HttpSession session) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), 
+                    "用户未登录，需要强制登录status=10");
+        }
+        return iUserService.getInformation(user.getId());
     }
 
 }
