92d30510029af76a8f57e25928d6b3486b161c67
CitrusMaxima <2663753900@qq.com>
2018-07-24 22:18:51 +0800

    前台查询商品列表功能实现

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -1,5 +1,9 @@
 package com.mmall.common;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
 public class Const {
 
     public static final String CURRENT_USER = "currentUser";
@@ -7,6 +11,10 @@ public class Const {
     public static final String EMAIL = "email";
     
     public static final String USERNAME = "username";
+    
+    public interface ProductListOrderBy{
+        Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_desc", "price_asc");
+    }
 
     public interface Role {
         int ROLE_CUSTOMER = 0;  // 普通用户
