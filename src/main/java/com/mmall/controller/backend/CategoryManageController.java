a4258b5c74178aaf919a8a0751d3dc809124d908
CitrusMaxima <2663753900@qq.com>
2018-07-24 13:19:24 +0800

    更新品类名字功能实现

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -48,4 +48,29 @@ public class CategoryManageController {
             return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
         }
     }
+
+    /**
+     * 更新品类名字
+     *
+     * @param session
+     * @param categoryId
+     * @param categoryName
+     * @return
+     */
+    @RequestMapping("update_category_name.do")
+    @ResponseBody
+    public ServerResponse updateCategoryName(HttpSession session, Integer categoryId, String categoryName) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录");
+        }
+        // 校验用户是否为管理员
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            // 是管理员
+            // 更新categoryName
+            return iCategoryService.updateCategoryName(categoryId, categoryName);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
+        }
+    }
 }
--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -5,5 +5,6 @@ import com.mmall.common.ServerResponse;
 public interface ICategoryService {
     
     ServerResponse addCategory(String categoryName, Integer parentId);
-    
+
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
 }
