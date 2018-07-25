418da7355a043e2529682e5c7b57540b30f701c4
CitrusMaxima <2663753900@qq.com>
2018-07-25 16:53:49 +0800

    收货地址模块参数修改

--- src/main/java/com/mmall/controller/portal/ShippingController.java
+++ src/main/java/com/mmall/controller/portal/ShippingController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/shippping")
+@RequestMapping("/shipping")
 public class ShippingController {
 
     @Autowired
