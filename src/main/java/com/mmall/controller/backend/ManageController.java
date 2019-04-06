34c4c1a217385ab559df60dca5330a1753ac4b99
CitrusMaxima <2663753900@qq.com>
2019-04-06 21:36:48 +0800

    统计用户数量、商品数量和订单数量

--- src/main/java/com/mmall/controller/backend/ManageController.java
+++ src/main/java/com/mmall/controller/backend/ManageController.java
@@ -1,48 +1,25 @@
 package com.mmall.controller.backend;
 
-import com.google.common.collect.Lists;
 import com.mmall.common.ServerResponse;
-import com.mmall.service.IOrderService;
-import com.mmall.service.IProductService;
-import com.mmall.service.IUserService;
-import com.mmall.service.impl.UserServiceImpl;
+import com.mmall.service.IManageService;
 import com.mmall.vo.ManageCountVo;
-import net.sf.jsqlparser.schema.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/manage/statistic")
 public class ManageController {
     
     @Autowired
-    IUserService iUserService;
-    
-    @Autowired
-    IProductService iProductService;
-    
-    @Autowired
-    IOrderService iOrderService;
+    IManageService iManageService;
 
-    @RequestMapping(value = "/base_count.do", method = RequestMethod.POST)
+    @RequestMapping(value = "/base_count.do", method = RequestMethod.GET)
     @ResponseBody
     public ServerResponse<ManageCountVo> baseCount() {
-        int userCount = iUserService.getUserCount();
-        int productCount = iProductService.getProductCount();
-        int orderCount = iOrderService.getOrderCount();
-
-        ManageCountVo manageCountVo = new ManageCountVo();
-
-        manageCountVo.setUserCount(userCount);
-        manageCountVo.setProductCount(productCount);
-        manageCountVo.setOrderCount(orderCount);
-        
-        return ServerResponse.createBySuccess(manageCountVo);
+        return iManageService.getBaseCount();
     }
 
 }
